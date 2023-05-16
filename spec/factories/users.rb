c3e00dc1290a37c87f37d993f537658b5c1fe017
sugimotohayato <sugimoto.seibunsya@gmail.com>
2023-05-16 18:57:38 +0900

    factorybotのpassword部分訂正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
 
     name {Faker::Name.name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
 
     first_name {japanese_user.first.kanji}
