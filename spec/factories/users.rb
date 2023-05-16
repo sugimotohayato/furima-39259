6bfbf4b75a338fa9ff85524cb8a9fe2445dd0545
sugimotohayato <sugimoto.seibunsya@gmail.com>
2023-05-16 17:54:05 +0900

    usersモデル単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    japanese_user = Gimei.name
+
+    name {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+
+    first_name {japanese_user.first.kanji}
+    last_name {japanese_user.last.kanji }
+    first_name_kana {japanese_user.first.katakana}
+    last_name_kana {japanese_user.last.katakana}
+    birth {Faker::Date.birthday}
+  end
+end
