49197e17933aa0886117547a56b44deba0b53cab
sugimotohayato <sugimoto.seibunsya@gmail.com>
2023-05-17 17:34:20 +0900

    苗字名前のヴァリテーション訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
          validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers"}
          validates :name, presence: true
-         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]/, message: "is invalid. Input full-width characters"}
-         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]/, message: "is invalid. Input full-width characters"}
+         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "is invalid. Input full-width characters"}
+         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "is invalid. Input full-width characters"}
          validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"}
          validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"}
          validates :birth, presence: true
