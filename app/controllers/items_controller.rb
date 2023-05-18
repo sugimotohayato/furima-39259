0a1e9de1e83c1e7fbd6509560e291b59bf16e128
sugimotohayato <sugimoto.seibunsya@gmail.com>
2023-05-18 12:44:13 +0900

    itemsコントローラーnew・create 編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new(item_params)
   end
 
   def create
-    item = Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
