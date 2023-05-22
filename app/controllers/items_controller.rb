86cc64557e43628863ff63c8fc8aab39ed516dd5
sugimotohayato <sugimoto.seibunsya@gmail.com>
2023-05-22 16:52:50 +0900

    set_itemにdestroyアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_sign_in, only: [:new, :edit, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -35,8 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
