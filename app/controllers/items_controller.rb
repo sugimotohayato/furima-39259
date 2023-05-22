ab11e94060960573d588e28af6df96d1ba7347de
sugimotohayato <sugimoto.seibunsya@gmail.com>
2023-05-22 14:37:01 +0900

    edit&updateアクション編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,20 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+        redirect_to item_path
+    else
+      @item.update(item_params)
+      render :edit
+    end
+  end
 
   private
 
