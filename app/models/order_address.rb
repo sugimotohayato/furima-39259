a3fa8b62dd8b469f2d41f964ecd5ece897c8deca
sugimotohayato <sugimoto.seibunsya@gmail.com>
2023-05-24 18:29:38 +0900

    OrderAddress設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,5 +1,23 @@
 class OrderAddress
   include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :state_id, :city, :address, :building, :tel, :order_id
   
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :state_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :address
+    validates :tel, numericality: {only_integer: true, message: "tel is invalid. Input half-width numbers"}
+    validates :order_id
+  end
+
+  def save
+    order = Order.create(user_id: current_user.id, item_id: item_id)
+    # 住所を保存する
+    # donation_idには、変数donationのidと指定する
+    Address.create(postcode: postacode, state_id: state_id, city: city, address: address, building: building, tel: tel, :order_id: order.id)
+  end
 
 end
